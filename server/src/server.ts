887f4c6eaedbbef2d3271f0e577afef079d4369e
Matt Mullen <mhmullen@icloud.com>
2025-04-28 20:25:13 -0700

    removed path line 2 from server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import path from "node:path";
+// import path from "node:path";
 import type { Request, Response } from "express";
 // Import the ApolloServer class
 import { ApolloServer } from "@apollo/server";
