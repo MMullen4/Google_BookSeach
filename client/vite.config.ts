3378c1c25c9ef1321be845dc0dc8775b35245e49
Matt Mullen <mhmullen@icloud.com>
2025-04-29 13:16:05 -0700

    mods to vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,17 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/graphql': { // plugs into back end
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
+  ...(mode === 'development' && {
+    server: {
+      port: 3000,
+      open: true,
+      proxy: {
+        '/graphql': { // plugs into back end
+          target: 'http://localhost:3001',
+          secure: false,
+          changeOrigin: true
+        }
       }
     }
-  }
-})
+  })
+}))
